d3f6315bf1cbf71b917cf7c0e23c18ad1fac49fc
Stephen Clay <sclay@datix.co.uk>
2016-11-29 09:58:15 +0000

    Commented out code temporarily

--- src/WebApplication4/Logic/PayPalFunctions.cs
+++ src/WebApplication4/Logic/PayPalFunctions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Specialized;
 using System.IO;
 using System.Net;
@@ -320,4 +320,4 @@ public sealed class NVPCodec : NameValueCollection
         }
         return name + index;
     }
-}
+}*/
